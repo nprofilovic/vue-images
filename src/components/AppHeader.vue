43161ccaffb81b4f77bb0eddd78aad128b36accd
nprofilovic <nprofilovic@gmail.com>
2020-01-21 12:58:42 +0100

    Make some init files

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="ui secondary pointing menu">
+      <a href="/" class="active item">
+        Image Storage
+      </a>
+     
+      <div class="right menu">
+        <a class="ui item">
+          Log Out
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
+
+<style scoped>
+
+</style>
